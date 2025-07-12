8a00c5e63bce2110dc6698193edcc552b08699b2
NoLag-ui <francisfranza10@gmail.com>
2025-07-12 16:57:50 +0800

    Update GrowagardenScripts.lua

--- GrowagardenScripts.lua
+++ GrowagardenScripts.lua
@@ -1,2 +1,7 @@
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/xfM7Ae5f8b/0", true))()
+task.spawn(function()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/xfM7Ae5f8b/0", true))()
+end)
