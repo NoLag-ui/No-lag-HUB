9ddaa805576423c8192c91e888e451f03aeeb9a2
NoLag-ui <francisfranza10@gmail.com>
2025-07-12 16:16:36 +0800

    Delete Loader/LoaderV1.lua

--- Loader/LoaderV1.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/xfM7Ae5f8b/0", true))()
-loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
