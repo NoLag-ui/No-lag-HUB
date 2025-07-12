7b4efa23b09433973222966b67b9ff901545ef15
NoLag-ui <francisfranza10@gmail.com>
2025-07-12 16:01:53 +0800

    Create LoaderV1.lua

---
+++ Loader/LoaderV1.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/xfM7Ae5f8b/0", true))()
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
